1586ac0224cd942e4e5afcaa5c28809052d89702
Dharan-07 <dharanishdharan2135@gmail.com>
2026-08-17 16:11:32 +0530

    updated the code from hardcoded input to get input from user by terminal and also protobuf only accept binary for hard to give input in postman

--- clients/protobufClient.js
+++ clients/protobufClient.js
@@ -1,117 +1,89 @@
 const http = require("http");
 const protobuf = require("protobufjs");
+const readline = require("readline");
 
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
 async function sendUser() {
 
-    const root =
-        await protobuf.load("./schema/user.proto");
+    const root = await protobuf.load("./schema/user.proto");
 
+    const User = root.lookupType("User");
 
-    const User =
-        root.lookupType("User");
+    // Get input dynamically
+    rl.question("Enter name: ", (name) => {
 
+        rl.question("Enter age: ", async (ageInput) => {
 
-    const user = {
-        name: "Dharan",
-        age: 21
-    };
+            const user = {
+                name: name,
+                age: Number(ageInput)
+            };
 
+            // Create Protobuf message
+            const message = User.create(user);
 
-    // Create Protobuf message
-    const message =
-        User.create(user);
+            // Encode to binary
+            const buffer = User.encode(message).finish();
 
+            console.log("\nSending buffer:");
+            console.log(buffer);
 
-    // Encode to binary
-    const buffer =
-        User.encode(message).finish();
+            const options = {
+                hostname: "localhost",
+                port: 4000,
+                path: "/user",
+                method: "POST",
 
+                headers: {
+                    "Content-Type": "application/x-protobuf",
+                    "Content-Length": buffer.length
+                }
+            };
 
-    console.log("Sending buffer:");
-    console.log(buffer);
+            const req = http.request(options, (res) => {
 
+                const chunks = [];
 
-    const options = {
+                res.on("data", (chunk) => {
+                    chunks.push(chunk);
+                });
 
-        hostname: "localhost",
+                res.on("end", () => {
 
-        port: 4000,
+                    const responseBuffer =
+                        Buffer.concat(chunks);
 
-        path: "/user",
+                    console.log("\nServer response:");
+                    console.log(responseBuffer);
 
-        method: "POST",
+                    // Decode Protobuf response
+                    const decoded =
+                        User.decode(responseBuffer);
 
-        headers: {
+                    console.log("\nDecoded response:");
 
-            "Content-Type":
-                "application/x-protobuf",
-
-            "Content-Length":
-                buffer.length
-
-        }
-
-    };
-
-
-    const req =
-        http.request(options, (res) => {
-
-            const chunks = [];
-
-
-            res.on("data", (chunk) => {
-
-                chunks.push(chunk);
-
-            });
-
-
-            res.on("end", () => {
-
-                const responseBuffer =
-                    Buffer.concat(chunks);
-
-
-                console.log(
-                    "\nServer response:"
-                );
-
-                console.log(responseBuffer);
-
-
-                const decoded =
-                    User.decode(
-                        responseBuffer
+                    console.log(
+                        User.toObject(decoded)
                     );
 
-
-                console.log(
-                    "\nDecoded response:"
-                );
-
-                console.log(
-                    User.toObject(decoded)
-                );
-
+                    rl.close();
+                });
             });
 
+            req.on("error", (error) => {
+                console.log("Request error:", error);
+                rl.close();
+            });
+
+            req.write(buffer);
+
+            req.end();
         });
-
-
-    req.on("error", (error) => {
-
-        console.log(error);
-
     });
-
-
-    req.write(buffer);
-
-    req.end();
-
 }
-
 
 sendUser();
