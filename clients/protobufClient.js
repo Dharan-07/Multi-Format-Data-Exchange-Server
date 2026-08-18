1bf8516ecf94a225aa5285fa708a2de625843d16
Dharan-07 <dharanishdharan2135@gmail.com>
2026-08-18 14:01:24 +0530

    updating the protobuf client code

--- clients/protobufClient.js
+++ clients/protobufClient.js
@@ -1,5 +1,5 @@
 const http = require("http");
-const protobuf = require("protobufjs");
+const { serialize, deserialize } = require("../serializers/protobuf");
 const readline = require("readline");
 
 const rl = readline.createInterface({
@@ -9,81 +9,132 @@ const rl = readline.createInterface({
 
 async function sendUser() {
 
-    const root = await protobuf.load("./schema/user.proto");
-
-    const User = root.lookupType("User");
-
-    // Get input dynamically
     rl.question("Enter name: ", (name) => {
 
-        rl.question("Enter age: ", async (ageInput) => {
+        rl.question("Enter age: ", (ageInput) => {
 
             const user = {
                 name: name,
                 age: Number(ageInput)
             };
 
-            // Create Protobuf message
-            const message = User.create(user);
 
-            // Encode to binary
-            const buffer = User.encode(message).finish();
+            console.log("\nUser:");
+            console.log(user);
 
-            console.log("\nSending buffer:");
+
+            // --------------------------------
+            // Serialize
+            // --------------------------------
+
+            const buffer = serialize(user);
+
+
+            console.log("\nBuffer:");
             console.log(buffer);
+
+
+            console.log("\nBuffer length:");
+            console.log(buffer.length);
+
+
+            console.log("\nBuffer as array:");
+            console.log([...buffer]);
+
+
+            console.log("\nBuffer as hex:");
+            console.log(
+                buffer.toString("hex")
+            );
+
+
+            console.log("\nBuffer as binary:");
+
+            console.log(
+                [...buffer]
+                    .map(byte =>
+                        byte
+                            .toString(2)
+                            .padStart(8, "0")
+                    )
+                    .join(" ")
+            );
+
+
+            // --------------------------------
+            // HTTP Request
+            // --------------------------------
 
             const options = {
                 hostname: "localhost",
                 port: 4000,
                 path: "/user",
                 method: "POST",
-
                 headers: {
-                    "Content-Type": "application/x-protobuf",
-                    "Content-Length": buffer.length
+
+                    "Content-Type":
+                        "application/x-protobuf",
+
+                    "Content-Length":
+                        buffer.length
                 }
             };
 
-            const req = http.request(options, (res) => {
+            const req =
+                http.request(options, (res) => {
 
-                const chunks = [];
+                    const chunks = [];
 
-                res.on("data", (chunk) => {
-                    chunks.push(chunk);
+                    res.on("data", (chunk) => {
+
+                        chunks.push(chunk);
+
+                    });
+
+
+                    res.on("end", () => {
+
+                        const responseBuffer =
+                            Buffer.concat(chunks);
+
+
+                        console.log(
+                            "\nServer response:"
+                        );
+
+                        console.log(responseBuffer);
+
+
+                        // --------------------------------
+                        // Deserialize response
+                        // --------------------------------
+
+                        const decoded =
+                            deserialize(responseBuffer);
+
+
+                        console.log(
+                            "\nDecoded response:"
+                        );
+                        
+                        console.log(decoded);
+                        rl.close();
+                    });
                 });
-
-                res.on("end", () => {
-
-                    const responseBuffer =
-                        Buffer.concat(chunks);
-
-                    console.log("\nServer response:");
-                    console.log(responseBuffer);
-
-                    // Decode Protobuf response
-                    const decoded =
-                        User.decode(responseBuffer);
-
-                    console.log("\nDecoded response:");
-
-                    console.log(
-                        User.toObject(decoded)
-                    );
-
-                    rl.close();
-                });
-            });
-
             req.on("error", (error) => {
-                console.log("Request error:", error);
+
+                console.log(
+                    "Request error:",
+                    error
+                );
+
                 rl.close();
             });
-
+            // Send binary buffer
             req.write(buffer);
 
             req.end();
         });
     });
 }
-
 sendUser();
